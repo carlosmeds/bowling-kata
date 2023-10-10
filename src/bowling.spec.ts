26173c67bbf2eed42f9f843d51a6476f9d7803af
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2023-10-09 23:21:53 -0300

    test: use makeSut on tests

--- src/bowling.spec.ts
+++ src/bowling.spec.ts
@@ -1,29 +1,39 @@
 import { Game } from "./bowling"
 
+type SutTypes = {
+  sut: Game;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new Game()
+
+  return { sut }
+}
+
 describe("Bowling", () => {
   it("Should roll the ball successfully", () => {
-    const game = new Game()
-    const rollSpy = jest.spyOn(game, "roll")
+    const { sut } = makeSut()
+    const rollSpy = jest.spyOn(sut, "roll")
 
-    game.roll(0)
+    sut.roll(0)
 
     expect(rollSpy).toHaveBeenCalled()
   })
   it("Should throw an error if the number of pins is greater than 10", () => {
-    const game = new Game()
+    const { sut } = makeSut()
 
-    expect(() => game.roll(11)).toThrow("Invalid number of pins")
+    expect(() => sut.roll(11)).toThrow("Invalid number of pins")
   })
   it("Should throw an error if the number of pins is less than 0", () => {
-    const game = new Game()
+    const { sut } = makeSut()
 
-    expect(() => game.roll(-1)).toThrow("Invalid number of pins")
+    expect(() => sut.roll(-1)).toThrow("Invalid number of pins")
   })
   it("Should add the number of pins to the score", () => {
-    const game = new Game()
+    const { sut } = makeSut()
 
-    game.roll(5)
+    sut.roll(5)
 
-    expect(game.score).toEqual(5)
+    expect(sut.score).toEqual(5)
   })
 })
